838976756f5ea1f45f4538a27a5ff48f57b461dc
cthos <daginus@gmail.com>
2017-12-26 16:47:04 -0800

    Try the gitlab runner.

--- stencil/src/components.d.ts
+++ stencil/src/components.d.ts
@@ -8,6 +8,108 @@ import '@stencil/router';
 
 
 import {
+  CharacterAttribute as CharacterAttribute
+} from './components/character-sheet/character-attribute/character-attribute';
+
+declare global {
+  interface HTMLCharacterAttributeElement extends CharacterAttribute, HTMLElement {
+  }
+  var HTMLCharacterAttributeElement: {
+    prototype: HTMLCharacterAttributeElement;
+    new (): HTMLCharacterAttributeElement;
+  };
+  interface HTMLElementTagNameMap {
+    "character-attribute": HTMLCharacterAttributeElement;
+  }
+  interface ElementTagNameMap {
+    "character-attribute": HTMLCharacterAttributeElement;
+  }
+  namespace JSX {
+    interface IntrinsicElements {
+      "character-attribute": JSXElements.CharacterAttributeAttributes;
+    }
+  }
+  namespace JSXElements {
+    export interface CharacterAttributeAttributes extends HTMLAttributes {
+      attributeCap?: number;
+      attributeValue?: number;
+      bonusCalculation?: Function;
+      name?: string;
+      showBonus?: boolean;
+      styleType?: string;
+    }
+  }
+}
+
+
+import {
+  CharacterSkill as CharacterSkill
+} from './components/character-sheet/character-skill/character-skill';
+
+declare global {
+  interface HTMLCharacterSkillElement extends CharacterSkill, HTMLElement {
+  }
+  var HTMLCharacterSkillElement: {
+    prototype: HTMLCharacterSkillElement;
+    new (): HTMLCharacterSkillElement;
+  };
+  interface HTMLElementTagNameMap {
+    "character-skill": HTMLCharacterSkillElement;
+  }
+  interface ElementTagNameMap {
+    "character-skill": HTMLCharacterSkillElement;
+  }
+  namespace JSX {
+    interface IntrinsicElements {
+      "character-skill": JSXElements.CharacterSkillAttributes;
+    }
+  }
+  namespace JSXElements {
+    export interface CharacterSkillAttributes extends HTMLAttributes {
+      attributeBonus?: number;
+      bonus?: number;
+      name?: string;
+      proficiencyBonus?: number;
+      proficient?: boolean;
+      ranks?: number;
+    }
+  }
+}
+
+
+import {
+  PointTracker as PointTracker
+} from './components/character-sheet/point-tracker/point-tracker';
+
+declare global {
+  interface HTMLPointTrackerElement extends PointTracker, HTMLElement {
+  }
+  var HTMLPointTrackerElement: {
+    prototype: HTMLPointTrackerElement;
+    new (): HTMLPointTrackerElement;
+  };
+  interface HTMLElementTagNameMap {
+    "point-tracker": HTMLPointTrackerElement;
+  }
+  interface ElementTagNameMap {
+    "point-tracker": HTMLPointTrackerElement;
+  }
+  namespace JSX {
+    interface IntrinsicElements {
+      "point-tracker": JSXElements.PointTrackerAttributes;
+    }
+  }
+  namespace JSXElements {
+    export interface PointTrackerAttributes extends HTMLAttributes {
+      maximum?: number;
+      minimum?: number;
+      title?: string;
+    }
+  }
+}
+
+
+import {
   Wallet as RpgWallet
 } from './components/character-sheet/wallet/wallet';
 
@@ -33,6 +135,66 @@ declare global {
     export interface RpgWalletAttributes extends HTMLAttributes {
       currencies?: string[];
       currencyValues?: number[];
+    }
+  }
+}
+
+
+import {
+  EditableNumber as EditableNumber
+} from './components/common/editable-number/editable-number';
+
+declare global {
+  interface HTMLEditableNumberElement extends EditableNumber, HTMLElement {
+  }
+  var HTMLEditableNumberElement: {
+    prototype: HTMLEditableNumberElement;
+    new (): HTMLEditableNumberElement;
+  };
+  interface HTMLElementTagNameMap {
+    "editable-number": HTMLEditableNumberElement;
+  }
+  interface ElementTagNameMap {
+    "editable-number": HTMLEditableNumberElement;
+  }
+  namespace JSX {
+    interface IntrinsicElements {
+      "editable-number": JSXElements.EditableNumberAttributes;
+    }
+  }
+  namespace JSXElements {
+    export interface EditableNumberAttributes extends HTMLAttributes {
+      startingValue?: number;
+    }
+  }
+}
+
+
+import {
+  MyApp as MyApp
+} from './components/my-app/my-app';
+
+declare global {
+  interface HTMLMyAppElement extends MyApp, HTMLElement {
+  }
+  var HTMLMyAppElement: {
+    prototype: HTMLMyAppElement;
+    new (): HTMLMyAppElement;
+  };
+  interface HTMLElementTagNameMap {
+    "my-app": HTMLMyAppElement;
+  }
+  interface ElementTagNameMap {
+    "my-app": HTMLMyAppElement;
+  }
+  namespace JSX {
+    interface IntrinsicElements {
+      "my-app": JSXElements.MyAppAttributes;
+    }
+  }
+  namespace JSXElements {
+    export interface MyAppAttributes extends HTMLAttributes {
+      
     }
   }
 }
