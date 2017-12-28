875f4d2c06871b57aa77166b7055e2213b400e7c
cthos <daginus@gmail.com>
2017-12-27 16:05:41 -0800

    Wrap up the death saving throws.

--- stencil/src/components/5e/death-saving-throw/death-saving-throw.tsx
+++ stencil/src/components/5e/death-saving-throw/death-saving-throw.tsx
@@ -1,5 +1,8 @@
-import { Component, Prop, State } from '@stencil/core';
+import { Component, Prop, State, Method } from '@stencil/core';
 
+/**
+ * @todo: The dot display could probably be its own sub-component.
+ */
 @Component({
   styleUrl: './death-saving-throw.scss',
   tag: 'death-saving-throw'
@@ -11,10 +14,14 @@ export class DeathSavingThrow {
   @State() numSuccesses: number = 0;
   @State() numFailures: number = 0;
 
+  @Method()
+  clearCounts() {
+    this.numSuccesses = 0;
+    this.numFailures = 0;
+  }
+
   counterMap(num: number, tickedNum: number) {
     let mapArr = new Array(num).fill(false);
-
-    console.log(mapArr);
 
     return mapArr.map((_, i) => {
       if (i < tickedNum) {
@@ -24,33 +31,84 @@ export class DeathSavingThrow {
     });
   }
 
+  decrement(which) {
+    if (which === 'success') {
+      this.numSuccesses -= 1;
+    } else {
+      this.numFailures -= 1;
+    }
+
+    this.checkForImpossibleNumbers();
+  }
+
+  increment(which) {
+    if (which === 'success') {
+      this.numSuccesses += 1;
+    } else {
+      this.numFailures += 1;
+    }
+
+    this.checkForImpossibleNumbers();
+  }
+
+  checkForImpossibleNumbers() {
+    if (this.numSuccesses < 0) {
+      this.numSuccesses = 0;
+    }
+    if (this.numFailures < 0) {
+      this.numFailures = 0;
+    }
+
+    if (this.numSuccesses > this.maxSuccesses) {
+      this.numSuccesses = this.maxSuccesses;
+    }
+    if (this.numFailures > this.maxFailures) {
+      this.numFailures = this.maxFailures;
+    }
+  }
+
   render() {
     return (
       <div class="death-saving-throw-container">
         <div class="counter-container success-counter">
           <div class="counter-name">Successes</div>
-          <div class="counter-trackers">
-          {this.counterMap(this.maxSuccesses, this.numSuccesses).map((v) => {
-            if (v) {
-              return (<span class="filled"></span>);
-            }
 
-            return (<span class="unfilled"></span>);
-          })}
+          <div class="counter-trackers">
+            <div class="minus-button">
+              <button onClick={() => this.decrement('success')}>-</button>
+            </div>
+            {this.counterMap(this.maxSuccesses, this.numSuccesses).map((v) => {
+              if (v) {
+                return (<span class="filled"></span>);
+              }
+
+              return (<span class="unfilled"></span>);
+            })}
+            <div class="plus-button">
+              <button onClick={_ => this.increment('success')}>+</button>
+            </div>
           </div>
         </div>
         <div class="counter-container failure-counter">
           <div class="counter-name">Failures</div>
-          <div class="counter-trackers">
-          {this.counterMap(this.maxFailures, this.numFailures).map((v) => {
-            if (v) {
-              return (<span class="filled"></span>);
-            }
 
-            return (<span class="unfilled"></span>);
-          })}
+          <div class="counter-trackers">
+            <div class="minus-button">
+              <button onClick={() => this.decrement('failure')}>-</button>
+            </div>
+            {this.counterMap(this.maxFailures, this.numFailures).map((v) => {
+              if (v) {
+                return (<span class="filled"></span>);
+              }
+
+              return (<span class="unfilled"></span>);
+            })}
+            <div class="plus-button">
+              <button onClick={_ => this.increment('failure')}>+</button>
+            </div>
           </div>
         </div>
+        <h2>Death Saves</h2>
       </div>
     )
   }
